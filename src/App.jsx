cf042bc269443d537fe2d301c5ceb85e844fad1a
tapswisonu <sytapswi@gmail.com>
2025-04-17 14:49:52 +0530

    added charts in home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/OtherPage/NotFound";
 import AppLayout from "./layout/AppLayout";
 import Home from "./pages/Dashboard/Home";
+import { ScrollToTop } from "./components/common/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
+        {/* Dashboard Layout */}
         <Route element={<AppLayout />}>
-          <Route path="/" element={<Home/>} />
-
-        </Route>  
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
