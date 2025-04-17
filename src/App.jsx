e4dd929c834795760887df35a840ce60f6aaea9f
tapswisonu <sytapswi@gmail.com>
2025-04-17 15:39:33 +0530

    added userprofile page and form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NotFound from "./pages/OtherPage/NotFound";
 import AppLayout from "./layout/AppLayout";
 import Home from "./pages/Dashboard/Home";
 import { ScrollToTop } from "./components/common/ScrollToTop";
+import UserProfiles from "./pages/UserProfiles";
 
 function App() {
   return (
@@ -12,8 +13,32 @@ function App() {
       <Routes>
         {/* Dashboard Layout */}
         <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
+          <Route index path="/" element={<Home />} />
+          {/* Others Page */}
+          <Route path="/profile" element={<UserProfiles />} />
+          <Route path="/calendar" element={<Calendar />} /> 
+          {/* {/* <Route path="/blank" element={<Blank />} /> */}
+          {/* Forms */}
+          {/* <Route path="/form-elements" element={<FormElements />} /> */}
+          {/* Tables */}
+          {/* <Route path="/basic-tables" element={<BasicTables />} /> */}
+          {/* Ui Elements */}
+          {/* <Route path="/alerts" element={<Alerts />} />
+          <Route path="/avatars" element={<Avatars />} />
+          <Route path="/badge" element={<Badges />} />
+          <Route path="/buttons" element={<Buttons />} />
+          <Route path="/images" element={<Images />} />
+          <Route path="/videos" element={<Videos />} /> */}
+          {/* Charts */}
+          {/* <Route path="/line-chart" element={<LineChart />} />
+          <Route path="/bar-chart" element={<BarChart />} /> */}
         </Route>
+
+        {/* Auth Layout */}
+        {/* <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} /> */}
+
+        {/* Fallback Route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
